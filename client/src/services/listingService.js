bb3f5a4ab4d0563a6e9d0d6b460102484a8810e1
Heshie London <heshielondon@gmail.com>
2018-06-13 12:35:44 -0700

    updated api key ref

--- client/src/services/listingService.js
+++ client/src/services/listingService.js
@@ -11,7 +11,7 @@ export function createListingService(data, userId, callback){
     processData: false,
     contentType: false,
     headers: {
-      Authorization: 'Client-ID ' + IMGUR_API_KEY,
+      Authorization: 'Client-ID ' + IMGUR_API_ID,
       Accept: 'application/json'
     },
     mimeType: 'multipart/form-data',
@@ -85,7 +85,7 @@ export function updateListingService(listing, oldListing, callback){
     processData: false,
     contentType: false,
     headers: {
-      Authorization: 'Client-ID ' + IMGUR_API_KEY,
+      Authorization: 'Client-ID ' + IMGUR_API_ID,
       Accept: 'application/json'
     },
     mimeType: 'multipart/form-data',
