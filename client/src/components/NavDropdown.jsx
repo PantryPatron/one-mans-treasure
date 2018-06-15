22d3371bc7c9742d556cb4d49c017a33be355b94
JohnWebb4 <John.Webb94@gmail.com>
2018-06-14 17:29:14 -0700

    Pass whole user to user profile

--- client/src/components/NavDropdown.jsx
+++ client/src/components/NavDropdown.jsx
@@ -1,69 +1,67 @@
-import React, { Component } from 'react'
-import { Button, Dropdown, Menu, Divider, Segment, Icon, DropdownDivider } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Button, Dropdown, Menu, Divider, Segment, Icon, DropdownDivider } from 'semantic-ui-react';
 import ListingCreator from './ListingCreator.jsx';
 import MyListings from './profileComponents/MyListings.jsx';
 import ClaimListings from './profileComponents/ClaimListings.jsx';
 import Profile from './Profile.jsx';
 
 class NavDropdown extends Component {
-  constructor(props){
-    super(props);  
+  constructor(props) {
+    super(props);
   }
 
-  handleItemClick(e, { name }){
-    this.setState({ activeItem: name })
-  } 
+  handleItemClick(e, { name }) {
+    this.setState({ activeItem: name });
+  }
 
-  logoutHandler(){
+  logoutHandler() {
     this.props.logout();
   }
 
-  renderUserDropdown(){
-      return (
-        <Dropdown.Menu>
-          {this.props.session ? <Dropdown.Item>
-            <ListingCreator user={this.props.session.user}
-              createListing={this.props.createListing.bind(this)}/>
-          </Dropdown.Item> : false}
+  renderUserDropdown() {
+    return (
+      <Dropdown.Menu>
+        {this.props.session ? <Dropdown.Item>
+          <ListingCreator user={this.props.session.user}
+            createListing={this.props.createListing.bind(this)}/>
+        </Dropdown.Item> : false}
 
-          {this.props.session ? <Dropdown.Item>
-            <MyListings listings={this.props.listings}
-            user={this.props.session.user} 
+        {this.props.session ? <Dropdown.Item>
+          <MyListings listings={this.props.listings}
+            user={this.props.session.user}
             listingSelectHandler={this.props.listingSelectHandler.bind(this)}
             delete={this.props.delete.bind(this)} giveHandler={this.props.giveHandler.bind(this)}>
-            </MyListings>
-          </Dropdown.Item> : false}
+          </MyListings>
+        </Dropdown.Item> : false}
 
-          {this.props.session ? <Dropdown.Item>
-            <ClaimListings claimed={this.props.session.user.claimed}
+        {this.props.session ? <Dropdown.Item>
+          <ClaimListings claimed={this.props.session.user.claimed}
             user={this.props.session.user}
             listingSelectHandler={this.props.listingSelectHandler.bind(this)}>
-            </ClaimListings>
-          </Dropdown.Item> : false}
+          </ClaimListings>
+        </Dropdown.Item> : false}
 
-          {this.props.session ? <Dropdown.Item>
-            <Profile
-            user={this.props.session.user.username}
-            userId={this.props.session.user._id}
-            password={this.props.session.user.password}
-            />
-          </Dropdown.Item> : false}
+        {this.props.session ? <Dropdown.Item>
+          <Profile
+            user={this.props.session.user}
+          />
+        </Dropdown.Item> : false}
 
-          <DropdownDivider/>
-          {this.props.session ? <Dropdown.Item> 
-            <div className="ui item" onClick={this.logoutHandler.bind(this)}>
+        <DropdownDivider/>
+        {this.props.session ? <Dropdown.Item>
+          <div className="ui item" onClick={this.logoutHandler.bind(this)}>
             <Icon name="log out"/>Logout</div>
-          </Dropdown.Item> : false}
-        </Dropdown.Menu>
-        )
+        </Dropdown.Item> : false}
+      </Dropdown.Menu>
+    );
   }
 
   render() {
     return (
-    <Dropdown text='MENU' item icon='bars' className="ui dropdown">
+      <Dropdown text='MENU' item icon='bars' className="ui dropdown">
         {this.renderUserDropdown()}
-    </Dropdown>
-    )
+      </Dropdown>
+    );
   }
 }
 
